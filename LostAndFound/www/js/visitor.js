7cbb5ca74579b54f160c3d7e73bd20b0fe11aaad
y2ktan <chunmuntan82@gmail.com>
2017-12-25 00:22:44 +0800

    temporary using ugly javascript onclick event

--- LostAndFound/www/js/visitor.js
+++ LostAndFound/www/js/visitor.js
@@ -1,34 +1,33 @@
-var BaseApp = angular.module('BaseApp', []);
-BaseApp.controller('HomePageCtrl', function ($scope) {
-    $scope.NavigateToLogin = function () {
-        window.location = "login.html";
-    };
-
-    $scope.NavigateToVisitor = function () {
-        window.location = "visitor.html";
-    };
+﻿var BaseApp = angular.module('BaseApp', []);
+//BaseApp.service("baseServices", function () {
+//    this.ToggleSideBar = function () {
+//        $('#sidebar').toggleClass('active'); alert("active");
+//    };
+//});
+BaseApp.controller('VisitorPageCtrl', function ($scope) {
+    
 });
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         console.log('Received Event: ' + id);
     }
 };
