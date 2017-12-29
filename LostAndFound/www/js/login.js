5786dbf2ca2f5096be0e318683092e44fcc1ed1a
khorkmy <khorkmy@hotmail.com>
2017-12-30 01:03:59 +0800

    Patch up login features together with turning to angular

---
+++ LostAndFound/www/js/login.js
@@ -0,0 +1,139 @@
+var BaseApp = angular.module('BaseApp', []);
+
+BaseApp.controller('LoginPageCtrl', function ($scope) {
+
+    // Initial values ~~~~~~~~~~~~~
+    $scope.ag_login = {
+        password: "PASSWORD",
+        username: "USERNAME"
+    };
+
+    $scope.dummy_database_json_path = "../asset/user_data_info.json";
+
+
+
+
+    // APIs ~~~~~~~~~~~~~~~~~
+
+    // This use for the sign in button
+    $scope.proceed_signin = function () {
+
+        // This is to load initial data if there wasn't there
+        if (localStorage.getItem("user_data_set") === null)
+        {
+            var base_value_obj = {
+                "test": {
+                    "user_name": "test",
+                    "password": "123",
+                    "first_name": "test1",
+                    "last_name": "example",
+                    "id_passport": "123-456-60",
+                    "email": "test@yahoo.com",
+                    "street": "some street",
+                    "city": "some city",
+                    "post_code": "some code",
+                    "country": "Soem contry",
+                    "gentle": "unknown",
+                    "claim": [
+                        {
+                            "item": "Iphone 1",
+                            "found_when": "dd/mm/yyyy hh"
+                        }
+                    ]
+                },
+                "cmtan": {
+                    "user_name": "cmtan",
+                    "password": "123",
+                    "first_name": "Chun Mun",
+                    "last_name": "Tan",
+                    "id_passport": "S8076606H",
+                    "email": "info@ressphere.com",
+                    "street": "16 Sandilands Road",
+                    "city": "SINGAPORE",
+                    "post_code": "546080",
+                    "country": "SINGAPORE",
+                    "gentle": "male",
+                    "claim": [
+                        {
+                            "item": "Iphone 5",
+                            "found_when": "25/01/2018 2pm"
+                        }
+                    ]
+                }
+            };
+
+            //console.log(base_value_obj);
+            var base_value = JSON.stringify(base_value_obj);
+            localStorage.setItem('user_data_set', base_value);
+
+            console.log("Asserting initial data !!!!");
+        }
+
+        // Retreive stored user data
+        var user_data_set_obj = JSON.parse(localStorage.getItem('user_data_set'));
+        //console.log(user_data_obj);
+
+        // Check the username and password
+        var login_status = "false";
+        if ($scope.ag_login.username in user_data_set_obj)
+        {
+            var user_data_obj = user_data_set_obj[$scope.ag_login.username];
+
+            if ($scope.ag_login.password === user_data_obj["password"])
+            {
+                sessionStorage.setItem("user_name", $scope.ag_login.username);
+                sessionStorage.setItem("user_data", user_data_obj);
+                login_status = "true";
+                $scope.ag_login.error_display = "Login ~~~~";
+
+                //alert(JSON.stringify(user_data_set_obj[$scope.ag_login.username]));
+
+                // Diver to user page
+                window.location = 'visitor.html'
+            }
+            else
+            {
+                $scope.ag_login.error_display = "Passwrod is incorrect !!";
+            }
+            
+        }
+        else
+        {
+            $scope.ag_login.error_display = "User name not found !!";
+            
+        }
+
+        // Record login status
+        sessionStorage.setItem("login_status", login_status);
+
+        // Following is to cleanup local storage, debug purpose
+        //localStorage.removeItem('user_data_set');
+    };
+
+
+});
+
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        console.log('Received Event: ' + id);
+    }
+};
