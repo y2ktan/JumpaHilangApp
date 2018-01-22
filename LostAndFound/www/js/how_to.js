16cc979808a015fd2480a2e73b63045e21a88597
ChongHan90 <chlim1990@gmail.com>
2018-01-22 08:44:39 +0800

    Update Tutorial Page to suit latest changes.

--- LostAndFound/www/js/how_to.js
+++ LostAndFound/www/js/how_to.js
@@ -1,5 +1,3 @@
-var BaseApp = angular.module('BaseApp', []);
-
 BaseApp.controller('HowToPageCtrl', function ($scope) {
     // Initial value
     $scope.ag_header = {
@@ -7,33 +5,8 @@ BaseApp.controller('HowToPageCtrl', function ($scope) {
     };
 
     $scope.NavigateToVisitor = function () {
-        window.location = "visitor.html";
+        window.location = "#!/visitor"
     };
 
 
 });
-
-var app = {
-    // Application Constructor
-    initialize: function () {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        console.log('Received Event: ' + id);
-    }
-};
